816cbc6d92bd412ca67347a3e1a07cc8c4ab3c9a
ryan manthorp <ryanmanthorp@hotmail.co.uk>
2018-03-07 15:22:45 +0000

    .

---
+++ Fishing/Assets/timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class timer : MonoBehaviour
+{
+	float timeLeft = 60.0f;
+	public Text timeText;
+
+
+	void Update()
+	{
+		timeLeft -= Time.deltaTime;
+		//Debug.Log (timeLeft);
+		timeText.text = timeText.ToString ();
+		if(timeLeft < 0)
+		{
+			Debug.Log("gameOver");
+		}
+	}
+}
